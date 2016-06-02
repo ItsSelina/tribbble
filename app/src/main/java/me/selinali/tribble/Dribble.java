05802740f129462a0d802195187d8d1f2bda7e1f
Selina Li <li.selina123@gmail.com>
2016-06-01 21:47:10 -0400

    Start ShotCardView

--- app/src/main/java/me/selinali/tribble/Dribble.java
+++ app/src/main/java/me/selinali/tribble/Dribble.java
@@ -4,21 +4,21 @@ import retrofit2.Retrofit;
 
 public class Dribble {
 
-    public static volatile Dribble sInstance;
+  public static volatile Dribble sInstance;
 
-    public static Dribble instance() {
-        return sInstance == null ? sInstance = new Dribble() : sInstance;
-    }
+  public static Dribble instance() {
+    return sInstance == null ? sInstance = new Dribble() : sInstance;
+  }
 
-    private final Endpoints mEndpoints;
+  private final Endpoints mEndpoints;
 
-    private Dribble() {
-        mEndpoints = new Retrofit.Builder()
-                .baseUrl("https://api.dribbble.com/v1/")
-                .build()
-                .create(Endpoints.class);
-    }
+  private Dribble() {
+    mEndpoints = new Retrofit.Builder()
+        .baseUrl("https://api.dribbble.com/v1/")
+        .build()
+        .create(Endpoints.class);
+  }
 
-    private interface Endpoints {
-    }
+  private interface Endpoints {
+  }
 }
---
+++ app/src/main/java/me/selinali/tribble/ShotCardView.java
@@ -0,0 +1,14 @@
+package me.selinali.tribble;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+
+public class ShotCardView extends FrameLayout {
+
+  public ShotCardView(Context context) {
+    super(context);
+    inflate(context, R.layout.shot_card_view, this);
+  }
+
+  
+}
