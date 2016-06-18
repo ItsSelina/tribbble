809a7ed21b0777f6a9fa54d72d164db78ed1f701
Selina Li <li.selina123@gmail.com>
2016-06-17 23:47:13 -0400

    Use custom view in ArchiveAdapter

--- app/src/main/java/me/selinali/tribble/ui/archive/ArchiveAdapter.java
+++ app/src/main/java/me/selinali/tribble/ui/archive/ArchiveAdapter.java
@@ -2,11 +2,14 @@ package me.selinali.tribble.ui.archive;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import java.util.List;
 
+import me.selinali.tribble.R;
 import me.selinali.tribble.data.IonLoader;
 import me.selinali.tribble.data.PicassoLoader;
 import me.selinali.tribble.model.Shot;
@@ -28,19 +31,15 @@ public class ArchiveAdapter extends RecyclerView.Adapter<ArchiveAdapter.ShotView
 
   @Override
   public ShotViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    ImageView imageView = new ImageView(parent.getContext());
-    imageView.setAdjustViewBounds(true);
-    return new ShotViewHolder(imageView);
+    View itemView = LayoutInflater.from(mContext).inflate(R.layout.archived_item, parent, false);
+    return new ShotViewHolder(itemView);
   }
 
   @Override
   public void onBindViewHolder(ShotViewHolder holder, int position) {
-    ImageView view = (ImageView) holder.itemView;
     Shot shot = mShots.get(position);
     (shot.isAnimated() ? mIonLoader : mPicassoLoader)
-        .load(shot.getImages().getHighResImage(), view);
-    view.setOnClickListener(v ->
-        mContext.startActivity(ShotActivity.launchIntentFor(shot, mContext)));
+        .load(shot.getImages().getHighResImage(), holder.mShotImageView);
   }
 
   @Override
@@ -48,9 +47,27 @@ public class ArchiveAdapter extends RecyclerView.Adapter<ArchiveAdapter.ShotView
     return mShots.size();
   }
 
-  class ShotViewHolder extends RecyclerView.ViewHolder {
-    public ShotViewHolder(ImageView imageView) {
-      super(imageView);
+  class ShotViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
+
+    public ImageView mShotImageView;
+    public View mCheckmarkOverlay;
+
+    public ShotViewHolder(View view) {
+      super(view);
+      view.setOnClickListener(this);
+      view.setOnLongClickListener(this);
+      mShotImageView = (ImageView) view.findViewById(R.id.imageview_shot);
+      mCheckmarkOverlay = view.findViewById(R.id.checkmark_overlay);
+    }
+
+    @Override
+    public void onClick(View v) {
+      mContext.startActivity(ShotActivity.launchIntentFor(mShots.get(getAdapterPosition()), mContext));
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+      return false;
     }
   }
 }
