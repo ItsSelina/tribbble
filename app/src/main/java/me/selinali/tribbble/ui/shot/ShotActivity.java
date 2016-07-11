b9f0ecf46ff256d2f898658eecf999cb95d01a5e
Selina Li <li.selina123@gmail.com>
2016-07-10 22:09:18 -0400

    Extract shot details into view

--- app/src/main/java/me/selinali/tribbble/ui/shot/ShotActivity.java
+++ app/src/main/java/me/selinali/tribbble/ui/shot/ShotActivity.java
@@ -4,24 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 import org.parceler.Parcels;
@@ -30,15 +28,12 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import de.hdodenhof.circleimageview.CircleImageView;
 import me.selinali.tribbble.R;
 import me.selinali.tribbble._;
 import me.selinali.tribbble.api.Dribble;
 import me.selinali.tribbble.model.Comment;
 import me.selinali.tribbble.model.Shot;
 import me.selinali.tribbble.ui.common.DividerItemDecoration;
-import me.selinali.tribbble.utils.DateUtils;
-import me.selinali.tribbble.utils.StringUtils;
 import me.selinali.tribbble.utils.ViewUtils;
 import rx.Observable;
 import rx.Subscription;
@@ -57,19 +52,8 @@ public class ShotActivity extends AppCompatActivity {
 
   @BindView(R.id.toolbar) Toolbar mToolbar;
   @BindView(R.id.shot_content_container) View mShotContentContainer;
+  @BindView(R.id.shot_details_view) ShotDetailsView mShotDetailsView;
   @BindView(R.id.imageview_shot) ImageView mShotImageView;
-  @BindView(R.id.textview_shot_name) TextView mShotNameTextView;
-  @BindView(R.id.textview_date) TextView mDateTextView;
-  @BindView(R.id.textview_likes_count) TextView mLikesTextView;
-  @BindView(R.id.textview_views_count) TextView mViewsTextView;
-  @BindView(R.id.textview_buckets_count) TextView mBucketsTextView;
-  @BindView(R.id.textview_comments_count) TextView mCommentsTextView;
-  @BindView(R.id.color_holder_left) LinearLayout mColorsPaneLeft;
-  @BindView(R.id.color_holder_right) LinearLayout mColorPaneRight;
-  @BindView(R.id.imageview_avatar) CircleImageView mAvatarImageView;
-  @BindView(R.id.textview_name) TextView mArtistName;
-  @BindView(R.id.textview_location) TextView mArtistLocation;
-  @BindView(R.id.textview_description) TextView mDescription;
   @BindView(R.id.recyclerview_comments) RecyclerView mCommentsRecyclerView;
   @BindView(R.id.progress_container) View mProgressContainer;
 
@@ -86,8 +70,8 @@ public class ShotActivity extends AppCompatActivity {
           .placeholder(R.drawable.grid_item_placeholder)
           .diskCacheStrategy(DiskCacheStrategy.SOURCE)
           .into(new GlideDrawableImageViewTarget(mShotImageView) {
-            @Override protected void setResource(GlideDrawable resource) {
-              super.setResource(resource);
+            @Override public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
+              super.onResourceReady(resource, animation);
               callback.call(((GlideBitmapDrawable) resource).getBitmap());
             }
           });
@@ -98,8 +82,8 @@ public class ShotActivity extends AppCompatActivity {
           .placeholder(R.drawable.grid_item_placeholder)
           .diskCacheStrategy(DiskCacheStrategy.SOURCE)
           .into(new GlideDrawableImageViewTarget(mShotImageView) {
-            @Override protected void setResource(GlideDrawable resource) {
-              super.setResource(resource);
+            @Override public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
+              super.onResourceReady(resource, animation);
               callback.call(((GifDrawable) resource).getFirstFrame());
             }
           });
@@ -119,7 +103,7 @@ public class ShotActivity extends AppCompatActivity {
 
     (mShot.isAnimated() ? mGifImageLoader : mStaticImageLoader).load(bitmap -> {
       Palette.from(bitmap).maximumColorCount(8)
-          .generate(palette -> bindSwatches(palette.getSwatches()));
+          .generate(palette -> mShotDetailsView.bind(palette.getSwatches()));
     });
 
     mShotSubscription = Dribble.instance()
@@ -143,34 +127,17 @@ public class ShotActivity extends AppCompatActivity {
 
   private void bindShot(Shot shot) {
     mShot = shot;
-    Glide.with(this).load(shot.getUser().getAvatarUrl()).into(mAvatarImageView);
-    mShotNameTextView.setText(shot.getTitle());
-    mDateTextView.setText(DateUtils.formatDate(shot.getCreatedAt()));
-    mLikesTextView.setText(String.valueOf(shot.getLikesCount()));
-    mViewsTextView.setText(String.valueOf(shot.getViewsCount()));
-    mBucketsTextView.setText(String.valueOf(shot.getBucketsCount()));
-    mCommentsTextView.setText(String.valueOf(shot.getCommentsCount()));
-    mArtistName.setText(shot.getUser().getName());
-    mArtistLocation.setText(shot.getUser().getLocation());
-    mDescription.setMovementMethod(LinkMovementMethod.getInstance());
-    mDescription.setText(StringUtils.trimTrailingNewLines(Html.fromHtml(shot.getDescription())));
+    mShotDetailsView.bind(shot);
     mCommentsRecyclerView.setNestedScrollingEnabled(false);
     mCommentsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     mCommentsRecyclerView.addItemDecoration(new DividerItemDecoration(this));
     mCommentsRecyclerView.setAdapter(new CommentsAdapter(shot.getComments()));
-    ViewUtils.fadeView(mProgressContainer, false, 150);
-  }
-
-  private void bindSwatches(List<Palette.Swatch> swatches) {
-    for (int i = 0; i < swatches.size(); i++) {
-      ColorView view = new ColorView(this, swatches.get(i).getRgb());
-      if (i % 2 == 0) mColorsPaneLeft.addView(view);
-      else mColorPaneRight.addView(view);
-    }
+    ViewCompat.animate(mShotContentContainer).alpha(1f).setDuration(200);
   }
 
   private void setupPadding() {
-    mShotContentContainer.setPadding(mShotContentContainer.getPaddingLeft(),
+    mShotContentContainer.setPadding(
+        mShotContentContainer.getPaddingLeft(),
         mShotContentContainer.getPaddingTop(),
         mShotContentContainer.getPaddingRight(),
         ViewUtils.getNavigationBarHeight());
---
+++ app/src/main/java/me/selinali/tribbble/ui/shot/ShotDetailsView.java
@@ -0,0 +1,73 @@
+package me.selinali.tribbble.ui.shot;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.graphics.Palette;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+import me.selinali.tribbble.R;
+import me.selinali.tribbble.model.Shot;
+import me.selinali.tribbble.utils.DateUtils;
+import me.selinali.tribbble.utils.StringUtils;
+
+public class ShotDetailsView extends RelativeLayout {
+
+  @BindView(R.id.textview_shot_name) TextView mShotNameTextView;
+  @BindView(R.id.textview_date) TextView mDateTextView;
+  @BindView(R.id.textview_likes_count) TextView mLikesTextView;
+  @BindView(R.id.textview_views_count) TextView mViewsTextView;
+  @BindView(R.id.textview_buckets_count) TextView mBucketsTextView;
+  @BindView(R.id.textview_comments_count) TextView mCommentsTextView;
+  @BindView(R.id.color_holder_left) LinearLayout mColorsPaneLeft;
+  @BindView(R.id.color_holder_right) LinearLayout mColorPaneRight;
+  @BindView(R.id.imageview_avatar) CircleImageView mAvatarImageView;
+  @BindView(R.id.textview_name) TextView mArtistNameTextView;
+  @BindView(R.id.textview_location) TextView mArtistLocationTextView;
+  @BindView(R.id.textview_description) TextView mDescriptionTextView;
+
+  public ShotDetailsView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    inflate(context, R.layout.shot_details_view, this);
+    ButterKnife.bind(this);
+    setBackgroundColor(Color.parseColor("#fffafafa"));
+  }
+
+  public void bind(Shot shot) {
+    Glide.with(getContext()).load(shot.getUser().getAvatarUrl()).into(mAvatarImageView);
+    mShotNameTextView.setText(shot.getTitle());
+    mDateTextView.setText(DateUtils.formatDate(shot.getCreatedAt()));
+    mLikesTextView.setText(String.valueOf(shot.getLikesCount()));
+    mViewsTextView.setText(String.valueOf(shot.getViewsCount()));
+    mBucketsTextView.setText(String.valueOf(shot.getBucketsCount()));
+    mCommentsTextView.setText(String.valueOf(shot.getCommentsCount()));
+    mArtistNameTextView.setText(shot.getUser().getName());
+    mArtistLocationTextView.setText(shot.getUser().getLocation());
+    mDescriptionTextView.setMovementMethod(LinkMovementMethod.getInstance());
+    String description = shot.getDescription();
+    if (description == null) {
+      mDescriptionTextView.setVisibility(GONE);
+    } else {
+      mDescriptionTextView.setText(StringUtils.trimTrailingNewLines(Html.fromHtml(description)));
+    }
+  }
+
+  public void bind(List<Palette.Swatch> swatches) {
+    for (int i = 0; i < swatches.size(); i++) {
+      ColorView view = new ColorView(getContext(), swatches.get(i).getRgb());
+      if (i % 2 == 0) mColorsPaneLeft.addView(view);
+      else mColorPaneRight.addView(view);
+    }
+  }
+}
